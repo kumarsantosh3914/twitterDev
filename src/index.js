8085f53770946275c8026aba63f0ed0d6e0fdcfd
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-03-16 18:02:16 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -10,13 +10,4 @@ app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('mongo db connected');
-    // const tweet = await Tweet.create({
-    //     content: 'Third tweet',
-    //     // userEmail: 'santosh@gmail.com'
-    // });
-    // const tweets = await Tweet.find();
-    // console.log(tweets);
-    const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.getAll(2, 4);
-    console.log(tweet);
 })
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,21 +4,9 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
+        max: [250, 'Tweet cannot be more than 250 characters']
     },
-    userEmail: {
-        type: String,
-    },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
 }, { timestamps: true });
-
-tweetSchema.virtual('contentWithEmail').get(function process() {
-    return `${this.content} \nCreated by: ${this.userEmail}`;
-})
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
