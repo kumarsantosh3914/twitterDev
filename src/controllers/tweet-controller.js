7c21d4eabfbc17ff24fff5d55b4c36b33b157709
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-03-17 02:51:23 +0530

    added controller and routes for create tweet api

---
+++ src/controllers/tweet-controller.js
@@ -0,0 +1,22 @@
+import TweetService from '../servies/tweet-service.js';
+
+const tweetService = new TweetService();
+
+export const createTweet = async (req, res) => {
+    try {
+        const response = await tweetService.create(req.body);
+        return res.status(201).json({
+            success: true,
+            message: 'Successfully created a new tweet',
+            data: response,
+            err: {}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Something went wrong',
+            data: {},
+            err: error,
+        });
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import { connect } from './config/database.js';
 
-const app = express();
+import apiRoutes from './routes/index.js';
 
-import service from './servies/tweet-service.js';
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use('/api', apiRoutes);
 
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('mongo db connected');
-    let ser = new service();
-    await ser.create({ content: 'capital #coDE ?' });
 });
