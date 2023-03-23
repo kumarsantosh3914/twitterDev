5fe880869b356c63a9e2d1ad097f4a07c253615c
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-03-23 23:23:18 +0530

    Added image upload in tweet api

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,10 +1,22 @@
 import TweetService from '../servies/tweet-service.js';
 
+import upload from '../config/file-upload-s3-config.js';
+
+const singleUploader = upload.single('image');
+
 const tweetService = new TweetService();
 
 export const createTweet = async (req, res) => {
     try {
-        const response = await tweetService.create(req.body);
+        singleUploader(req, res, function (err, data) {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            console.log('Image url is:', req.file);
+        });
+        const payload = { ...req.body };
+        payload.image = req.file.location;
+        const response = await tweetService.create(payload);
         return res.status(201).json({
             success: true,
             message: 'Successfully created a new tweet',
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -17,7 +17,10 @@ const tweetSchema = new mongoose.Schema({
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
         }
-    ]
+    ],
+    images: {
+        type: String,
+    }
 }, { timestamps: true });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
